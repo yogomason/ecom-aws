cc30f86b66ebeac778c95512a9c7830d62266913
yogomason <66168985+yogomason@users.noreply.github.com>
2020-05-31 14:59:15 +0100

    Update settings.py

--- ecomsite/settings.py
+++ ecomsite/settings.py
@@ -80,14 +80,11 @@ WSGI_APPLICATION = 'ecomsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'demo_test',
-        'USER':'postgres',
-        'PASSWORD':'snakee11',
-        'HOST':'localhost',
-        'PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
